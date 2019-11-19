7f56fa42af2fa884c8f6449b9fba6208bee94d47
famasoon <famasoon@gmail.com>
2019-11-19 23:17:21 +0900

    Add CTLogs type

---
+++ crtlog/ctlog.go
@@ -0,0 +1,11 @@
+package crtlog
+
+type CTLogs []struct {
+	IssuerCaID        int    `json:"issuer_ca_id"`
+	IssuerName        string `json:"issuer_name"`
+	NameValue         string `json:"name_value"`
+	MinCertID         int    `json:"min_cert_id"`
+	MinEntryTimestamp string `json:"min_entry_timestamp"`
+	NotBefore         string `json:"not_before"`
+	NotAfter          string `json:"not_after"`
+}

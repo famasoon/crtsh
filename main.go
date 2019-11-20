2907e93a78c7c509d38cc6780a8041265185ee90
famasoon <famasoon@gmail.com>
2019-11-20 21:49:33 +0900

    Add function for query

--- main.go
+++ main.go
@@ -14,6 +14,39 @@ import (
 // CRTSHURL is URL of crt.sh endpoint
 const CRTSHURL string = "https://crt.sh/"
 
+func queryCrt(query string) error {
+	res, err := http.Get(CRTSHURL + "?output=json&q=" + query)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != 200 {
+		err = fmt.Errorf("Can not Access crt.sh")
+		return err
+	}
+
+	var ctlogs crtlog.CTLogs
+
+	if err = json.NewDecoder(res.Body).Decode(&ctlogs); err != nil {
+		return err
+	}
+
+	for key, ctlog := range ctlogs {
+		fmt.Printf("Index: %d\n", key+1)
+		fmt.Printf("Issuer CA ID: %d\n", ctlog.IssuerCaID)
+		fmt.Printf("Issuer Name: %s\n", ctlog.IssuerName)
+		fmt.Printf("Name: %s\n", ctlog.NameValue)
+		fmt.Printf("Min Cert ID: %d\n", ctlog.MinCertID)
+		fmt.Printf("Min Entry TimeStamp: %s\n", ctlog.MinEntryTimestamp)
+		fmt.Printf("Not Before: %s\n", ctlog.NotBefore)
+		fmt.Printf("Not After: %s\n", ctlog.NotAfter)
+		fmt.Println()
+	}
+
+	return nil
+}
+
 // TODO: Create run function () (err)
 func main() {
 	var query string
@@ -30,33 +63,8 @@ func main() {
 
 	fmt.Println(query)
 
-	// TODO: Create HTTP Client
-	res, err := http.Get(CRTSHURL + "?output=json&q=" + query)
+	err := queryCrt(query)
 	if err != nil {
 		log.Fatal(err)
-	}
-	defer res.Body.Close()
-
-	if res.StatusCode != 200 {
-		err = fmt.Errorf("Can not Access crt.sh")
-		log.Fatal(err)
-	}
-
-	var ctlogs crtlog.CTLogs
-
-	if err = json.NewDecoder(res.Body).Decode(&ctlogs); err != nil {
-		log.Fatal(err)
-	}
-
-	for key, ctlog := range ctlogs {
-		fmt.Printf("Index: %d\n", key+1)
-		fmt.Printf("Issuer CA ID: %d\n", ctlog.IssuerCaID)
-		fmt.Printf("Issuer Name: %s\n", ctlog.IssuerName)
-		fmt.Printf("Name: %s\n", ctlog.NameValue)
-		fmt.Printf("Min Cert ID: %d\n", ctlog.MinCertID)
-		fmt.Printf("Min Entry TimeStamp: %s\n", ctlog.MinEntryTimestamp)
-		fmt.Printf("Not Before: %s\n", ctlog.NotBefore)
-		fmt.Printf("Not After: %s\n", ctlog.NotAfter)
-		fmt.Println()
 	}
 }

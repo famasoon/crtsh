d862317cecc794b9c159fb72baf275c3f6c0e3da
famasoon <famasoon@gmail.com>
2019-11-29 20:43:52 +0900

    Move query func to ctlog.go

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"crypto/x509"
-	"encoding/json"
 	"encoding/pem"
 	"flag"
 	"fmt"
@@ -11,14 +10,10 @@ import (
 	"net/http"
 	"os"
 	"strconv"
-	"time"
 
 	"github.com/famasoon/crtsh/crtlog"
 	"github.com/famasoon/crtsh/parser"
 )
-
-// CRTSHURL is URL of crt.sh endpoint
-const CRTSHURL string = "https://crt.sh/"
 
 func showUsage() {
 	fmt.Println("This tool shows the result of crt.sh")
@@ -30,93 +25,8 @@ func showUsage() {
 	os.Exit(0)
 }
 
-func queryCrtsh(query string) ([]byte, error) {
-	req := http.Client{
-		Timeout: 60 * time.Second,
-	}
-	res, err := req.Get(query)
-	if err != nil {
-		return nil, err
-	}
-	defer res.Body.Close()
-
-	if res.StatusCode != 200 {
-		err = fmt.Errorf("Can not Access crt.sh")
-		return nil, err
-	}
-
-	body, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		return nil, err
-	}
-
-	return body, nil
-}
-
-func searchComon(query string, onlyDomainFlag bool) error {
-	var ctlogs crtlog.CTLogs
-
-	res, err := queryCrtsh(CRTSHURL + "?output=json&CN=" + query)
-	if err = json.Unmarshal(res, &ctlogs); err != nil {
-		return err
-	}
-
-	if onlyDomainFlag {
-		for _, ctlog := range ctlogs {
-			fmt.Printf("%s\n", ctlog.NameValue)
-		}
-	} else {
-		for key, ctlog := range ctlogs {
-			fmt.Println("{")
-			fmt.Printf("  Index: %d\n", key+1)
-			fmt.Printf("  Issuer CA ID: %d\n", ctlog.IssuerCaID)
-			fmt.Printf("  Issuer Name: %s\n", ctlog.IssuerName)
-			fmt.Printf("  Name: %s\n", ctlog.NameValue)
-			fmt.Printf("  Min Cert ID: %d\n", ctlog.MinCertID)
-			fmt.Printf("  Min Entry TimeStamp: %s\n", ctlog.MinEntryTimestamp)
-			fmt.Printf("  Not Before: %s\n", ctlog.NotBefore)
-			fmt.Printf("  Not After: %s\n", ctlog.NotAfter)
-			fmt.Printf("  Donwload Pem file: %s?d=%d\n", CRTSHURL, ctlog.MinCertID)
-			fmt.Println("}")
-		}
-	}
-
-	return nil
-}
-
-func queryCrt(query string, onlyDomainFlag bool) error {
-	var ctlogs crtlog.CTLogs
-
-	res, err := queryCrtsh(CRTSHURL + "?output=json&q=" + query)
-	if err = json.Unmarshal(res, &ctlogs); err != nil {
-		return err
-	}
-
-	if onlyDomainFlag {
-		for _, ctlog := range ctlogs {
-			fmt.Printf("%s\n", ctlog.NameValue)
-		}
-	} else {
-		for key, ctlog := range ctlogs {
-			fmt.Println("{")
-			fmt.Printf("  Index: %d\n", key+1)
-			fmt.Printf("  Issuer CA ID: %d\n", ctlog.IssuerCaID)
-			fmt.Printf("  Issuer Name: %s\n", ctlog.IssuerName)
-			fmt.Printf("  Name: %s\n", ctlog.NameValue)
-			fmt.Printf("  Min Cert ID: %d\n", ctlog.MinCertID)
-			fmt.Printf("  Min Entry TimeStamp: %s\n", ctlog.MinEntryTimestamp)
-			fmt.Printf("  Not Before: %s\n", ctlog.NotBefore)
-			fmt.Printf("  Not After: %s\n", ctlog.NotAfter)
-			fmt.Printf("  Donwload Pem file: %s?d=%d\n", CRTSHURL, ctlog.MinCertID)
-			fmt.Println("}")
-		}
-	}
-
-	return nil
-}
-
 func parseCTLog(certID int) error {
-	res, err := http.Get(CRTSHURL + "?d=" + strconv.Itoa(certID))
+	res, err := http.Get(crtlog.CRTSHURL + "?d=" + strconv.Itoa(certID))
 	if err != nil {
 		return err
 	}
@@ -164,7 +74,7 @@ func main() {
 	}
 
 	if query != "" {
-		err := queryCrt(query, onlyDomainFlag)
+		err := crtlog.QueryCrt(query, onlyDomainFlag)
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -176,7 +86,7 @@ func main() {
 			log.Fatal(err)
 		}
 	} else {
-		err := searchComon(commonName, onlyDomainFlag)
+		err := crtlog.SearchComon(commonName, onlyDomainFlag)
 		if err != nil {
 			log.Fatal(err)
 		}
